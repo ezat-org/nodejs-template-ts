6e0e013d4abcec881ac8441aaae1bd16d34f6c48
lucaspan <panxiangyi@hotmail.com>
2023-05-26 21:41:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,84 +1,18 @@
-const cookie = require("cookie");
+// const middlewares = require("/opt/nodejs/middlewares");
+const middlewares = require("./doubleCookieSubmit");
+const middy = require('@middy/core');
 
-const defaults = {
-  csrfTokenCookieName: "X-CSRF-Token",
-  csrfTokenBodyKey: "csrfToken",
-};
-const doubleCookieSubmitMiddleware = (opts = {}) => {
-  const options = {
-    ...defaults,
-    ...opts,
-  };
-  const doubleCookieSubmitMiddlewareBefore = async (request) => {
-    const { headers, body, cookies, version } = request.event;
+const lambdaHandler = async (event, context) => {
+    const response = {
+        headers: {
+            "abc": 123
+        },
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+}
 
-    const httpMethod = getVersionHttpMethod[version ?? "1.0"]?.(request.event);
-    if (httpMethod !== "POST") return;
-
-    // Parse Cookies
-    let cookieObject = {};
-
-    // API Gateway V1
-    if (headers?.Cookie) {
-      cookieObject = cookie.parse(headers.Cookie);
-    }
-
-    // API Gateway V2
-    if (cookies && Array.isArray(cookies)) {
-      cookieObject = cookies.reduce(
-        (accumulator, cookieValue) => ({
-          ...accumulator,
-          ...cookie.parse(cookieValue),
-        }),
-        {}
-      );
-    }
-
-    // Parse JSON Body
-    let bodyObject = {};
-    try {
-      const data = request.event.isBase64Encoded
-        ? Buffer.from(body, "base64").toString()
-        : body;
-
-      bodyObject = JSON.parse(data);
-    } catch (err) {
-      console.log(err);
-      const response = {
-        statusCode: 400,
-        body: { message: "Invalid JSON body" },
-      };
-      return response;
-    }
-
-    const bodyCsrfToken = bodyObject
-      ? bodyObject[options.csrfTokenBodyKey]
-      : null;
-    const cookieCsrfToken = cookieObject
-      ? cookieObject[options.csrfTokenCookieName]
-      : null;
-
-    if (
-      !bodyCsrfToken ||
-      !cookieCsrfToken ||
-      bodyCsrfToken !== cookieCsrfToken
-    ) {
-      const response = {
-        statusCode: 403,
-        body: { message: "Invalid CSRF token" },
-      };
-      return response;
-    }
-  };
-
-  return {
-    before: doubleCookieSubmitMiddlewareBefore,
-  };
-};
-
-const getVersionHttpMethod = {
-  "1.0": (event) => event.httpMethod,
-  "2.0": (event) => event.requestContext.http.method,
-};
-
-exports.doubleCookieSubmitMiddleware = doubleCookieSubmitMiddleware;
+exports.handler = middy()
+  .use(middlewares.doubleCookieSubmitMiddleware()) // handles common http errors and returns proper responses
+  .handler(lambdaHandler)
